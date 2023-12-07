8638bfccd73d5c5f9a50da9323a391a3f8c04d95
Enrique Martinez <litepast@hotmail.com>
2023-12-07 10:39:07 -0600

    Second version of link builder, now only 1 function

--- link_builder.py
+++ link_builder.py
@@ -7,41 +7,12 @@ It will be redirected to a different website, appropriate to learn more about th
 # to return the first result of the exact link. Or even en scrapper if the site was 
 # too good and it did not have an API
 
-def movie_link_builder(title):    
-    youtube_link = f"https://www.youtube.com/results?search_query={title}+trailer"
-    return f'<a href="{youtube_link}">Watch the movie trailer on Youtube</a>'
-
-def album_link_builder(title):
-    spotify_link = f"https://open.spotify.com/search/{title}/albums"
-    return f'<a href="{spotify_link}">Listen to the Album on Spotify</a>'
-
-def videogame_link_builder(title):
-    moby_link = f"https://www.mobygames.com/search/?q={title}"
-    return f'<a href="{moby_link}">Check the videogame information on Mobygames</a>'
-
-def tv_link_builder(title):
-    imdb_link = f"https://www.imdb.com/find/?s=tt&q={title}&ref_=nv_sr_sm"
-    return f'<a href="{imdb_link}">Check the TV Show information on IMDB</a>'
-
-def book_link_builder(title):
-    goodreads_link = f"https://www.goodreads.com/search?utf8=%E2%9C%93&query={title}"
-    return f'<a href="{goodreads_link}">Check the Book information on GoodReads</a>'
-
-link_builder = {
-    0: album_link_builder,
-    1: videogame_link_builder,
-    2: movie_link_builder,
-    3: tv_link_builder,
-    4: book_link_builder,
-}
-
-def get_rec_link(title, index):
-    """
-    Gets the link to learn more about the recommendation.
-    Args:
-    - title (str): Recommendation title.
-    - index (int): The index of the media.
-    Returns:
-    - str: The anchor html tag to go to a website to learn about the recommendation.
-    """
-    return link_builder.get(index)(title)
+def get_rec_link2(title, index):
+    link_builder = {
+        0: '<a href="https://open.spotify.com/search/{}/albums">Listen to the Album on Spotify</a>',
+        1: '<a href="https://www.mobygames.com/search/?q={}">Check the videogame information on Mobygames</a>',        2: "",
+        2: '<a href="https://www.youtube.com/results?search_query={}+trailer">Watch the movie trailer on Youtube</a>',
+        3: '<a href="https://www.imdb.com/find/?s=tt&q={}&ref_=nv_sr_sm">Check the TV Show information on IMDB</a>',
+        4: '<a href="https://www.goodreads.com/search?utf8=%E2%9C%93&query={}">Check the Book information on GoodReads</a>'
+    }
+    return link_builder[index].format(title)
